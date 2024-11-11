c42086d5b116f7a4a6c9907fe21fff89e2ed5fe0
HSLU1 <hslu@MacBook.local>
2024-11-11 13:31:34 +0100

    Grundfunktionen

---
+++ pacman.c
@@ -0,0 +1,82 @@
+import random
+
+class PacmanGame:
+    def __init__(self):
+   
+    def select_map(self):
+        # Auswahl der Karte
+      
+
+    def load_level(self):
+        # Level laden und initialisieren
+ 
+
+    def generate_map(self):
+        
+
+    def print_map(self):
+
+
+    def touch_enemy(self):
+        # Spieler berhrt einen Gegner
+        pass
+
+    def collect_dot(self):
+        # Spieler sammelt einen Dot ein
+        pass
+
+    def end_game(self):
+        # Das Spiel endet, entweder durch Verlust aller Leben oder das Sammeln aller Dots
+        pass
+
+    def start_game(self):
+     
+    def move_pacman(self, direction):
+        # Pacman in die angegebene Richtung bewegen
+        pass
+
+    def teleport_pacman(self):
+        # Pacman an eine andere Position teleportieren
+        pass
+
+    def ghost_movement(self):
+        # Bewegung der Geister implementieren
+        pass
+
+    def check_collision(self):
+        # Kollision mit Wnden oder Gegnern berprfen
+        pass
+
+    def update_map(self):
+        # Karte nach jeder Bewegung aktualisieren
+        pass
+
+    def update_score(self):
+        # Punktestand aktualisieren
+        pass
+
+    def display_status(self):
+        # Spielstatus anzeigen (z.B. verbleibende Leben, Punktestand)
+        pass
+
+    def check_win_condition(self):
+        # berprfen, ob der Spieler gewonnen hat (z.B. alle Dots gesammelt)
+        pass
+
+    def pause_game(self):
+        # Das Spiel pausieren
+        pass
+
+    def power_up_collected(self):
+        # Power-Up einsammeln und Effekt aktivieren
+        pass
+
+    def respawn_pacman(self):
+        # Pacman nach Verlust eines Lebens wieder an der Startposition erscheinen lassen
+        pass
+
+# Spiel starten
+if __name__ == "__main__":
+    game = PacmanGame()
+    game.start_game()
+
